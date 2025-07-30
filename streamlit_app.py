096f13a6eaa48381a78b1d16952d24f82ac8a20e
khusairimkj <85659615+khusairimkj@users.noreply.github.com>
2025-07-30 19:19:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
@@ -29,12 +30,12 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect('Chosse up to 5 ingredient :', my_dataframe, max_selections=5)
 
-import requests
+
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
+
+#smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
 if ingredients_list:
     #st.write(ingredients_list)
@@ -44,6 +45,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
 
